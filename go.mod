542fa6518e51aae1583d70274e5ecddbfa2dad78
Alexandre Setta <alexsetta@gmail.com>
2022-07-22 09:55:41 -0300

    Executa somente nos horários pré-definidos

--- go.mod
+++ go.mod
@@ -6,3 +6,9 @@ require (
 	github.com/alexsetta/httputils v0.0.0-20220721135358-917cf22b6d11
 	github.com/alexsetta/telegram v0.0.0-20220719150426-384de71efef0
 )
+
+require (
+	github.com/go-co-op/gocron v1.15.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+)
--- megasena.go
+++ megasena.go
@@ -4,36 +4,56 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/alexsetta/httputils"
+	"github.com/go-co-op/gocron"
 	"log"
 	"time"
 )
 
 const (
-	url   = "https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena"
-	sleep = 60 * time.Minute
+	url = "https://servicebus2.caixa.gov.br/portaldeloterias/api/megasena"
 )
 
-var (
-	ultimo = ""
-)
+var ultimo = ""
+
+func taskTeste() {
+	log.Println("ok")
+}
+
+func task() {
+	data, err := httputils.GetBody(url, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var res map[string]interface{}
+	json.Unmarshal([]byte(data), &res)
+
+	concurso := fmt.Sprintf("%v", res["numero"])
+	if ultimo != concurso {
+		ultimo = concurso
+		notifica(concurso, fmt.Sprintf(" %v", res["listaDezenas"]))
+	}
+}
 
 func main() {
-	for {
-		data, err := httputils.GetBody(url, false)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var res map[string]interface{}
-		json.Unmarshal([]byte(data), &res)
-
-		concurso := fmt.Sprintf("%v", res["numero"])
-		if ultimo != concurso {
-			ultimo = concurso
-			notifica(concurso, fmt.Sprintf(" %v", res["listaDezenas"]))
-		}
-
-		log.Println("Aguardando próxima execução")
-		time.Sleep(sleep)
+	s := gocron.NewScheduler(time.UTC)
+	location, err := time.LoadLocation("America/Sao_Paulo")
+	if err != nil {
+		log.Fatalf("Error loading location: %s", err)
 	}
+	s.ChangeLocation(location)
+
+	job, err := s.Every(1).Day().Wednesday().Saturday().At("20:15;20:30").Do(task)
+	//job, err := s.Every(10).Seconds().Do(taskTeste)
+	if err != nil {
+		log.Fatalf("Error creating job: %v", err)
+	}
+	go func() {
+		for {
+			time.Sleep(10 * time.Second)
+			log.Println("Next run", job.NextRun())
+			time.Sleep(12 * time.Hour)
+		}
+	}()
+	s.StartBlocking()
 }
